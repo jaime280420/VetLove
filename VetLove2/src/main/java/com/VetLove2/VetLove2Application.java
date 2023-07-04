68914a62af5de193f8d2e2e258519fefbcd093b6
jaime280420 <133932647+jaime280420@users.noreply.github.com>
2023-07-04 00:12:41 -0600

    Se crea Repositorio
    Fundamentos de desarrollo web

---
+++ VetLove2/src/main/java/com/VetLove2/VetLove2Application.java
@@ -0,0 +1,13 @@
+package com.VetLove2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetLove2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetLove2Application.class, args);
+	}
+
+}
