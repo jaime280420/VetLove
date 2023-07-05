faaeec7df003cbf90c083f877660eac1a35b0ccb
Axel Noel Cabezas Cerda <114128602+AxelCab17@users.noreply.github.com>
2023-07-04 23:02:08 -0600

    Reinicio total

--- VetLove2/src/main/java/com/VetLove2/dao/RegistroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.VetLove2.dao;
-
-import com.VetLove2.domain.Registro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegistroRepository extends JpaRepository<Registro, Long> {
-}
